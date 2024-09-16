835ca306ab7e11ee6e18e3c7c2c5a6b756c4e7ba
sjkim994 <sjkd2022@mymail.pomona.edu>
2024-09-16 17:48:40 +0200

    player horizontal movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,14 +3,21 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private Rigidbody2D _rb;
+
     void Start()
     {
-        
+        _rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movePlayer();
+    }
+
+    private void movePlayer() {
+        _rb.linearVelocityX = Input.GetAxisRaw("Horizontal");
     }
 }
