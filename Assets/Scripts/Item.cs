c61d82b43ab5dd551ecaeb09e6c06e4c41cfd88a
Levi Neuwirth <ln@levineuwirth.org>
2024-09-23 17:24:14 +0200

    None item infrastructure

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 
 public class Item : MonoBehaviour {
     [Header("Properties")]
+    // Determines if the item is beneficial to the recipe or not.
+    // To be set on the basis of the current level.
     public bool isTrash;
+    // Determines if this is the None item.
+    // If true, this is what we use to implement pauses in spawning via the ItemSpawner.
+    public bool isNone;
+    // This has to be public. The ItemSpawner needs to know where the item can spawn.
+    // Entries here also have to be in the ItemSpawner's spawn locations array.
+    public Vector3[] allowedLocations;
     // other properties that we haven't thought of will go here...
 
     public void onSubmit(){
 	// if it's trash, bad, if it isn't, good!
+	// we may move this logic to the player depending on the final design
+	// (it isn't a priority right now)
     }
 }
--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -6,17 +6,22 @@ public class ItemSpawner: MonoBehaviour {
     [Header("Level-Specific Data")]
     public Vector3[] positions;
     public Item[] spawnableItems;
+    // this Item represents when nothing should spawn. Special case.
+    // be sure to NOT add this to the spawnableItems. Only here!
+    public Item noneItem;
     public float spawnDuration;
     
     void Start(){
 	Queue<Item> spawns = new Queue<Item>();
 	foreach (Item item in spawnableItems){
 	    spawns.Enqueue(item);
+	    spawns.Enqueue(noneItem);
 	}
 	// Queue is now initialized. Send off a coroutine for the spawning loop?
     }
 
     void Update(){
 	// anything here?
+	// lean towards starting this logic in a coroutine launched at the end of Start()
     }
 }
