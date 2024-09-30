3f7993756aa0c725c4edce0498a3519d57864c6b
sjkim994 <sjkd2022@mymail.pomona.edu>
2024-09-30 13:26:02 +0200

    updated tutorial UI

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     
-    private float healthValue;
-    private 
+    [field: SerializeField] public float healthValue {get ; private set;}
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
